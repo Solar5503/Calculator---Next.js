cf57c3c8f2f33ebe35173829d71aded3c70ae7b7
solar <spidervvv@mail.ru>
2022-01-22 21:57:23 +0300

    add components

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import headerStyles from '../styles/Header.module.css';
+
+const Header = () => {
+  //   const x = 5;
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>Give </span>credit
+      </h1>
+      <p className={headerStyles.description}>Do you need money? Come to us!</p>
+      {/* <style jsx>
+        {`
+          .title {
+            color: ${x > 3 ? 'red' : 'blue'};
+          }
+        `}
+      </style> */}
+    </div>
+  );
+};
+
+export default Header;
